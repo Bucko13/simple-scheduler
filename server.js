05dffb1372dcc00507a983d01a024ef7e1e87b0b
Bucko <bucko.perley@gmail.com>
2016-06-18 16:22:49 -0700

    server and initial client setup

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan'); // for console logging with express 4
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.json());                                     // parse application/json
+
+app.get('/', (req, res) => {
+  res.send('index.html');
+});
+
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('App listening on port '.concat(port));
+});
